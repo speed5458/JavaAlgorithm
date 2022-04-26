97080911e853f96b62022d03eea7d1881ed31fab
최영준 <44596010+speed5458@users.noreply.github.com>
2022-04-26 21:12:44 +0900

    07 팰린드롬 (완료)
    StringBuilder

--- StringStudy/src/String07.java
+++ StringStudy/src/String07.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class String07 {
     public static void main(String[] args){
+        String rsAnswer = "YES";
+        Scanner sc = new Scanner(System.in);
+        String inputS1 = sc.next();
+        String reverseInputS1 = new StringBuilder(inputS1).reverse().toString();
+
+
+        if(inputS1.equalsIgnoreCase(reverseInputS1)){
+            rsAnswer = "YES";
+        }else{
+            rsAnswer = "NO";
+        }
+
+        System.out.println(rsAnswer);
+
 
     }
 }
