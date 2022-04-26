95bb181b106c27c9a3bc5676e2db7358ad58e072
최영준 <44596010+speed5458@users.noreply.github.com>
2022-04-26 21:41:36 +0900

    8. 유효한 팰린드롬 (완료)
    정규식 사용
    [^A-Z]

--- StringStudy/src/String08.java
+++ StringStudy/src/String08.java
@@ -1,2 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class String08 {
+    public static void main(String[] args){
+        String rsAnswer = "YES";
+        Scanner sc = new Scanner(System.in);
+        String inputS1 = sc.nextLine();
+
+        inputS1 = inputS1.toUpperCase().replaceAll("[^A-Z]","");
+        String reverseInpuS1 = new StringBuilder(inputS1).reverse().toString();
+
+        if(!reverseInpuS1.equalsIgnoreCase(inputS1)){
+            rsAnswer = "NO";
+        }
+
+        System.out.println(rsAnswer);
+
+    }
 }
