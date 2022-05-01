18e81aaa6eed0963afa09b7b54b3fd0c95e7c202
최영준 <44596010+speed5458@users.noreply.github.com>
2022-05-01 10:11:49 +0900

    Array 06. 뒤집은소수
    숫자 뒤집기 후 각각 소수가 맞는지 확인

--- StringStudy/src/ArrayStydy/Array06.java
+++ StringStudy/src/ArrayStydy/Array06.java
@@ -1,5 +1,44 @@
 package ArrayStydy;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Array06 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arrC = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrC[i] = sc.nextInt();
+        }
+
+        for (int x : solution(n,arrC)) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+    private static ArrayList<Integer> solution(int n, int[] arrC) {
+        ArrayList answer = new ArrayList<>();
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            num = arrC[i];
+            int reverseNum = 0;
+            while (num != 0) {
+                int digit = num % 10;
+                reverseNum = reverseNum * 10 + digit;
+                num /= 10;
+            }
+            if(isPrime(reverseNum)) answer.add(reverseNum);
+        }
+        return answer;
+    }
+
+    private static boolean isPrime(int num) {
+        if(num == 1) return false;
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
 }
