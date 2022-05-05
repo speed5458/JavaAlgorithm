dbb22d57db7753674c45474e18190eee4aaad3a5
최영준 <44596010+speed5458@users.noreply.github.com>
2022-05-05 11:08:32 +0900

    Array12 멘토링 4중for문
    for i 기준사람
    for j 비교사람
    for k 문제횟수
    for s i j 해당 인덱스 찾아 등수 비교

---
+++ StringStudy/src/ArrayStydy/Array12.java
@@ -0,0 +1,41 @@
+package ArrayStydy;
+
+import java.util.Scanner;
+
+public class Array12 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 4 열 학생수
+        int m = sc.nextInt(); //3 행 테스트수
+        int[][] arrInt = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arrInt[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print(solution(arrInt, n, m));
+    }
+
+    public static int solution(int[][] arrInt, int n, int m){
+        int answer = 0;
+        for (int i = 1; i <= n; i++) { // 기준 사람군
+            for (int j = 1; j <= n; j++) { //비교 사람군
+                int cnt = 0;
+                for (int k = 0; k < m; k++) { //테스트 횟수
+                    int pi = 0;
+                    int pj = 0;
+                    for (int s = 0; s < n; s++) { // 등수 확인
+                        if(arrInt[k][s] == i) pi = s;
+                        if(arrInt[k][s] == j) pj = s;
+                    }
+                    if(pi < pj){
+                        cnt++;
+                    }
+                }//end k
+             if(cnt == m) answer++;
+            } //end j
+        } //end i
+
+        return answer;
+    }
+}
