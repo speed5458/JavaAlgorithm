40a5299898fe80ff783081354d291e171ed19f2c
최영준 <44596010+speed5458@users.noreply.github.com>
2022-06-20 21:22:25 +0900

    BFS 추가

--- StringStudy/src/RecursiveTreeGraph/ExamDFS.java
+++ StringStudy/src/RecursiveTreeGraph/ExamDFS.java
@@ -1,5 +1,8 @@
 package RecursiveTreeGraph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node{
     int data;
     Node lt, rt;
@@ -10,7 +13,6 @@ class Node{
     }
 }
 public class ExamDFS {
-
     Node root;
     public void DFS(Node root){
         if(root==null) return;
@@ -23,6 +25,26 @@ public class ExamDFS {
         }
     }
 
+    public void BFS(Node root){
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()){
+            int len = Q.size();
+            System.out.print(L + " : ");
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                System.out.print(cur.data + " ");
+                if(cur.lt != null) Q.offer(cur.lt);
+                if(cur.rt != null) Q.offer(cur.rt);
+            }
+            L++;
+            System.out.println();
+
+
+        }
+    }
+
     public static void main(String[] args){
         ExamDFS tree = new ExamDFS();
         tree.root = new Node(1);
@@ -32,7 +54,9 @@ public class ExamDFS {
         tree.root.lt.rt = new Node(5);
         tree.root.rt.lt = new Node(6);
         tree.root.rt.rt = new Node(7);
-        tree.DFS(tree.root);
+        //tree.DFS(tree.root);
+
+        tree.BFS(tree.root);
     }
 
 }
