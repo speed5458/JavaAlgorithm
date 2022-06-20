c98609e70ac56d124a7ee9161c4fbe24cdb01516
최영준 <44596010+speed5458@users.noreply.github.com>
2022-06-20 21:58:05 +0900

    8.송아지찾기(BFS) 완료
    1. int [] dis = {1, -1, 5} 를 넣어서
    처음 노드 5에서부터 레벨별로 탐색해 나아간다.
    2. 방문한 좌표는 ch[s] = 1 로 처리해서 속도를 올린다. 메모이제이션
    3. 계산된 좌표들은  Que 에 넣어서 다시 for문돌면서 노드 탐색 한다.

--- StringStudy/src/RecursiveTreeGraph/송아지찾기.java
+++ StringStudy/src/RecursiveTreeGraph/송아지찾기.java
@@ -1,12 +1,41 @@
 package RecursiveTreeGraph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class 송아지찾기 {
     public static void main(String[] args){
-
+        송아지찾기 T = new 송아지찾기();
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+        System.out.println(T.BFS(s, e));
     }
-    public static int solution(int[] arrI){
-        int answer = 0;
-
-        return answer;
+    int[] dis = {1, -1, 5}; //콩콩으로 이동할수있는 종류
+    int[] ch; //방문한 좌표 입력
+    Queue<Integer> Q = new LinkedList<>();
+    public int BFS(int s, int e){
+        ch = new int[10001];
+        ch[s] = 1; //방문한 좌표는 1로 둔다.
+        Q.offer(s);
+        int L = 0;
+        while(!Q.isEmpty()){
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                int x = Q.poll();
+                if (x == e) return L;
+                for (int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if(nx >= 1 && nx <= 10000 && ch[nx] == 0){ //좌표의 조건 확인및 방문했는지 확인한다.
+                        //첫방문시 로직
+                        ch[nx] = 1;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
     }
 }
