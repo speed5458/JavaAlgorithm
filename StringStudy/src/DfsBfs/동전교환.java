1ba8701e42a0daf1b2df9473d2279ef939cfaff3
최영준 <44596010+speed5458@users.noreply.github.com>
2022-07-10 13:14:28 +0900

    5. 동전교환(완료) DFS
    DFS(L+1 , sum + arr[i]); //sum += arr[i] 로 두면 값이 왜 4가 나옴??????????

---
+++ StringStudy/src/DfsBfs/동전교환.java
@@ -0,0 +1,42 @@
+package DfsBfs;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 동전교환 {
+    static int n, m;
+    static Integer[] arr;
+    static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args){
+        동전교환 T = new 동전교환();
+
+        Scanner sc= new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+
+
+        m = sc.nextInt();
+        T.DFS(0, 0);
+        System.out.println(answer);
+    }
+
+    public void DFS(int L, int sum){
+        if(sum > m) return;
+        if(L >= answer) return;
+        if(sum == m) {
+            answer = Math.min(L, answer);
+        }else{
+            for (int i = 0; i < n; i++) {
+                DFS(L+1 , sum + arr[i]); //sum += arr[i] 로 두면 값이 왜 4가 나옴??????????
+            }
+        }
+
+    }
+}
